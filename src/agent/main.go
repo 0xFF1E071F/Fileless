b344ae153bfd21aa2cb64871b35df218ba67f89e
noxie <evarist.verstraete@gmail.com>
2022-10-23 12:33:05 -0400

    Uncommented stuff in main.go (agent)

--- src/agent/main.go
+++ src/agent/main.go
@@ -2,13 +2,9 @@ package main
 
 func main() {
 	initRecon()
-	/*
 	if (arrayToString(sysInfo.Machine) == "x86" || arrayToString(sysInfo.Machine) == "x86_64") {
 		initNode()
 	} else {
-		initClient()
+		callBootstrap()
 	}
-	*/
-
-	callBootstrap()
 }
