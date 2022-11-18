9a8ccba1d2201a706456f108c665ee3ea67f3e8a
nox <evarist.verstraete@student.howest.be>
2022-11-18 10:58:29 +0100

    Implemented signing of commandw

--- src/interface/crypto.go
+++ src/interface/crypto.go
@@ -1,37 +1,58 @@
-/*
-  TODO:
-  
-  - Implementing asymmetric encryption (ed25519)
-  - Documentation: https://pkg.go.dev/crypto/ed25519
-*/
-
 package main
 
 import (
+  "fmt"
   "io/ioutil"
-  crypto "crypto/ed25519"
   "encoding/base64"
+
+  crypto "crypto/ed25519"
+  "crypto/md5"
 )
 
+var PubKey crypto.PublicKey
+var PrivKey crypto.PrivateKey
+var PubKeyBase string
+var PrivKeyBase string
+
 func generateKeypairs() {
+  /*
+    TODO:
+
+    - Optimizing the body of this function
+  */
   pubKey, privKey, err := crypto.GenerateKey(nil)
   CheckErr(err)
 
-  pubKeyBase := base64.StdEncoding.EncodeToString([]byte(pubKey))
-  privKeyBase := base64.StdEncoding.EncodeToString([]byte(privKey))
+  PubKey = pubKey
+  PrivKey = privKey
+
+  PubKeyBase = base64.StdEncoding.EncodeToString([]byte(PubKey))
+  PrivKeyBase = base64.StdEncoding.EncodeToString([]byte(PrivKey))
   // Writing the keypairs to the keys folder
 
-  err = ioutil.WriteFile("keys/ed25519.pub", []byte(pubKeyBase), 0644)
+  err = ioutil.WriteFile("keys/ed25519.pub", []byte(PubKeyBase), 0644)
   CheckErr(err)
 
-  err = ioutil.WriteFile("keys/ed25519", []byte(privKeyBase), 0644)
+  err = ioutil.WriteFile("keys/ed25519", []byte(PrivKeyBase), 0644)
   CheckErr(err)
+}
+
+func SignCmd(cmd string) string {
+  /* 
+    TODO:
+    
+    - Optimizing the body of this function
+  */
+
+  fmt.Println("Private key: ", PrivKey)
+  hashSlice := md5.Sum([]byte(cmd))
+  cmdMD5 := hashSlice[:]
+  signedCmd := crypto.Sign(PrivKey, cmdMD5)
+
+  return base64.StdEncoding.EncodeToString(signedCmd) 
 }
 
 func InitCrypto() {
   generateKeypairs()
 }
 
-func main() {
-  InitCrypto()
-}
